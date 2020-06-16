e304e613af661525cfaf433bbb54ab0a252f437c
ignaes_Lionel@hotmail.com <ignaes_lionel@hotmail.com>
2020-06-16 21:06:05 +0200

    changement fichier html et css

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import MyInput from './MyInput'
 function App() {
   return (
     <div className="App">
-      <h1>HELLO WORLD</h1>
+      <h1>HELLO WORLD, first commit</h1>
       <MyInput/>
     </div>
   );
